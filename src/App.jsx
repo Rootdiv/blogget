7315ba845a3f0de0a3c24bbcf4e475093e0cfa52
Rootdiv <rootdiv@mail.ru>
2022-09-07 15:42:04 +0300

    feat: added JSX components, styles & hygen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Header from './components/Header';
+import Main from './components/Main';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello React!</p>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+    </>
   );
 }
 
