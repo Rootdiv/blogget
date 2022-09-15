8da828d9c46c5bccf1ed88684b2d6042ede65b59
Rootdiv <rootdiv@mail.ru>
2022-09-15 13:10:55 +0300

    feat: show form and set browser focus in textarea using Refs

--- src/components/Modal/FormComment/FormComment.jsx
+++ src/components/Modal/FormComment/FormComment.jsx
@@ -1,16 +1,29 @@
 import style from './FormComment.module.css';
-import { useContext, useRef } from 'react';
+import { useState, useContext, useRef, useEffect } from 'react';
 import { authContext } from 'context/authContext';
 import { Text } from 'UI/Text';
 
 export const FormComment = () => {
+  const [showForm, setShowForm] = useState(false);
   const { auth } = useContext(authContext);
   const textareaRef = useRef(null);
+
+  useEffect(() => {
+    if (showForm) {
+      textareaRef.current.focus();
+    }
+  }, [showForm]);
+
+  const handleClick = () => {
+    setShowForm(true);
+  };
+
   const commentSubmit = event => {
     event.preventDefault();
     console.log(textareaRef.current.value);
   };
-  return (
+
+  return showForm ? (
     <form className={style.form} onSubmit={commentSubmit}>
       <Text As="h3" size={14} tsize={18}>
         {auth.name}
@@ -18,5 +31,9 @@ export const FormComment = () => {
       <textarea className={style.textarea} ref={textareaRef}></textarea>
       <button className={style.btn}>Отправить</button>
     </form>
+  ) : (
+    <button className={style.btn} onClick={handleClick}>
+      Написать комментарий
+    </button>
   );
 };
