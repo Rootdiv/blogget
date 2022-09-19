17a0b177a23770885d315f17d30a068c09ca3ded
Rootdiv <rootdiv@mail.ru>
2022-09-19 16:50:54 +0300

    fix: posts Preloader

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,4 +1,5 @@
 import { useBestPosts } from 'hooks/useBestPost';
+import Preloader from 'UI/Preloader';
 import { Text } from 'UI/Text';
 import style from './List.module.css';
 import Post from './Post';
@@ -8,7 +9,7 @@ export const List = () => {
 
   return (
     <ul className={style.list}>
-      {loading ? <Text>Загрузка...</Text> : posts.length ? (
+      {loading ? <Preloader color='#56AF27' size={250} /> : posts.length ? (
         posts.map(({ data: postData }) => (<Post key={postData.id} postData={postData} />))
       ) : (
         <Text>Вы не авторизованы или Постов не найдено</Text>
