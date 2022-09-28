40e74d9bc0deb15c32472757b028ad8fc10ba3cb
Rootdiv <rootdiv@mail.ru>
2022-09-28 11:24:05 +0300

    feat: React project deployment on Vercel

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'cpyz4nuvJHsNCXTDjr2jjg';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'SpecialistWorker2582';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogget-blush.vercel.app/auth';
 export const SCOPE = 'identity submit read';
